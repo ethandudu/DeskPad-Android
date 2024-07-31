3a434e9143a83303a97210c248b3731d2e59c652
Ethan <41381482+ethandudu@users.noreply.github.com>
2024-07-31 12:06:22 +0200

    Switched to Websocket communication

--- app/src/main/java/fr/ethanduault/deskpad/MainActivity.java
+++ app/src/main/java/fr/ethanduault/deskpad/MainActivity.java
@@ -4,9 +4,11 @@ import static fr.ethanduault.deskpad.Utils.getSHA256;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +21,11 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
+import java.net.URI;
 import java.security.NoSuchAlgorithmException;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -29,10 +34,12 @@ import fr.ethanduault.deskpad.Utils;
 public class MainActivity extends AppCompatActivity {
 
     private static final String SERVER_PASSWORD;
+    private WebSocketClient webSocketClient;
 
     static {
         try {
             SERVER_PASSWORD = getSHA256("password");
+            System.out.println("Password"+SERVER_PASSWORD);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         } catch (UnsupportedEncodingException e) {
@@ -55,46 +62,97 @@ public class MainActivity extends AppCompatActivity {
         buttonF13.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMessage("key","F13");
+                //sendMessage("key","F13");
             }
         });
+
+
         Button buttonCommand = findViewById(R.id.buttonCommand);
         buttonCommand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMessage("run","explorer.exe");
+                //sendMessage("run","explorer.exe");
+            }
+        });
+
+        buttonCommand.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Long click", Toast.LENGTH_SHORT).show();
+                //sendMessage("run","calc.exe");
+                return true;
+            }
+        });
+
+
+        Button buttonConnect = findViewById(R.id.buttonConnect);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText ipAddress = findViewById(R.id.ipAddress);
+                EditText port = findViewById(R.id.ipPort);
+                createWebSocketClient(ipAddress.getText().toString(), Integer.parseInt(port.getText().toString()));
             }
         });
     }
 
-    private void sendMessage(String type, String message) {
-        new SendMessageTask().execute(type, message);
-    }
-
-    private class SendMessageTask extends AsyncTask<String, Void, Void> {
-        @Override
-        protected Void doInBackground(String... params) {
-            JSONObject data = new JSONObject();
-            try {
-                data.put("password", SERVER_PASSWORD);
-                data.put("type", params[0]);
-                data.put("message", params[1]);
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-            EditText ipAddress = findViewById(R.id.ipAddress);
-            EditText ipPort = findViewById(R.id.ipPort);
-            System.out.println(ipAddress.getText().toString());
-            System.out.println(ipPort.getText().toString());
-            try (Socket socket = new Socket(ipAddress.getText().toString(), Integer.parseInt(ipPort.getText().toString()));
-
-
-                 PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
-                out.println(data.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+    private void createWebSocketClient(String address, int port) {
+        URI uri;
+        try {
+            uri = new URI("ws://" + address + ":" + port);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
         }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                Log.i("WebSocket", "Connecting");
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, "Connecting", Toast.LENGTH_SHORT).show());
+                JSONObject data = new JSONObject();
+                try {
+                    data.put("type", "auth");
+                    data.put("password", SERVER_PASSWORD);
+                    send(data.toString());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onMessage(String message) {
+                JSONObject data;
+                try {
+                    data = new JSONObject(message);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    if (data.getString("type").equals("auth")) {
+                        if (data.getBoolean("success")) {
+                            runOnUiThread(() -> Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show());
+                        } else {
+                            runOnUiThread(() -> Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show());
+                        }
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                Log.i("WebSocket", "Closed " + reason);
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, "Closed " + reason, Toast.LENGTH_SHORT).show());
+            }
+
+            @Override
+            public void onError(Exception ex) {
+
+            }
+        };
+        webSocketClient.connect();
     }
 }
